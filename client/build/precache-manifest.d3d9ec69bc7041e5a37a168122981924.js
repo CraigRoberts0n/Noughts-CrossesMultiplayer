d9e90c00687b7a9581190977c1a0c7c259a80070
CraigRoberts0n <CraigRobertson99@gmail.com>
2020-10-26 19:58:30 +0000

    build

---
+++ client/build/precache-manifest.d3d9ec69bc7041e5a37a168122981924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a080e7471a56c9e0d908344df80f9b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa5eff43954a3e1ff8b",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fed677bcbf62c34979b4",
+    "url": "/static/css/main.27c697c7.chunk.css"
+  },
+  {
+    "revision": "cfa5eff43954a3e1ff8b",
+    "url": "/static/js/2.5d77cd0a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.5d77cd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed677bcbf62c34979b4",
+    "url": "/static/js/main.2d4e9813.chunk.js"
+  },
+  {
+    "revision": "9e8c9be9e3ce975e88cf",
+    "url": "/static/js/runtime-main.8c756e2f.js"
+  }
+]);
